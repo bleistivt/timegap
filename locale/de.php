171cad585b462cbd63bebe6cd03f9c9d5335a41f
bleistivt <bleistivt@users.noreply.github.com>
2015-06-30 18:00:37 +0200

    Create de.php

---
+++ locale/de.php
@@ -0,0 +1,7 @@
+<?php
+
+$Definition['%s days later'] = '%s Tage später';
+$Definition['1 month later'] = 'Einen Monat später';
+$Definition['%s months later'] = '%s Monate später';
+$Definition['1 year later'] = 'Ein Jahr später';
+$Definition['%s years later'] = '%s Jahre später';
