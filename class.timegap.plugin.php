00c0c6d9c3dab0c9b7d09c9759aefa17e3e272d4
bleistivt <bleistivt@users.noreply.github.com>
2015-06-30 19:02:33 +0200

    shorten

--- class.timegap.plugin.php
+++ class.timegap.plugin.php
@@ -13,21 +13,18 @@ $PluginInfo['timegap'] = array(
 
 class TimeGapPlugin extends Gdn_Plugin {
 
+    private $previous = false;
+
     public function assetModel_styleCss_handler($sender) {
         $sender->addCssFile('timegap.css', 'plugins/timegap');
     }
 
     public function discussionController_beforeCommentDisplay_handler($sender, $args) {
         // Find the previous comment.
-        $comments = $sender->data('Comments');
-        $comments->dataSeek(-1);
-        while ($comment = $comments->nextRow()) {
-            if ($comment->CommentID == $args['Comment']->CommentID) break;
-        }
-        if (!$prev = $comments->previousRow()) {
-            $prev = $args['Discussion'];
+        if (!$this->previous) {
+            $this->previous = $args['Discussion'];
             if ($sender->data('Page', 1) != 1) {
-                $prev = $sender->CommentModel
+                $this->previous = $sender->CommentModel
                     ->get($sender->DiscussionID, 1, $sender->Offset - 1)
                     ->firstRow();
             }
@@ -35,28 +32,28 @@ class TimeGapPlugin extends Gdn_Plugin {
 
         // Calculate the date difference.
         $date = new DateTime($args['Comment']->DateInserted);
-        $gap = $date->diff(new DateTime($prev->DateInserted))->days ?: 0;
-        $class = 'TimeGap ';
+        $gap = $date->diff(new DateTime($this->previous->DateInserted))->days;
+        $this->previous = $args['Comment'];
 
-        if ($gap < 10) {
+        if ($gap < 6) {
             return;
         } if ($gap < 30) {
             $gap = sprintf(t('%s days later'), (int)$gap);
-            $class .= 'Days';
+            $class = 'Days';
         } elseif ($gap < 60) {
             $gap = t('1 month later');
-            $class .= 'Month';
+            $class = 'Month';
         } elseif ($gap < 365) {
             $gap = sprintf(t('%s months later'), (int)($gap / 30));
-            $class .= 'Months';
+            $class = 'Months';
         } elseif ($gap < 720) {
             $gap = t('1 year later');
-            $class .= 'Year';
+            $class = 'Year';
         } else {
             $gap = sprintf(t('%s years later'),  (int)($gap / 365));
-            $class .= 'Years';
+            $class = 'Years';
         }
-        echo wrap(sprite('Time', 'InformSprite').$gap, 'li', array('class' => $class));
+        echo wrap(sprite('Time', 'InformSprite').$gap, 'li', array('class' => 'TimeGap '.$class));
     }
 
 }
