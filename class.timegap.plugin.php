46980e31300e989be51f6b6e97ed15e0e7c58d37
bleistivt <bleistivt@users.noreply.github.com>
2021-02-09 20:52:41 +0100

    Remove shorthand function usage

--- class.timegap.plugin.php
+++ class.timegap.plugin.php
@@ -26,22 +26,22 @@ class TimeGapPlugin extends Gdn_Plugin {
         $gap = $date->diff(new DateTime($this->previous->DateInserted))->days;
         $this->previous = $args['Comment'];
 
-        if ($gap < c('TimeGap.MinDays', 7) - 1) {
+        if ($gap < Gdn::config('TimeGap.MinDays', 7) - 1) {
             return;
         } elseif ($gap < 30) {
-            $gap = sprintf(t('%s days later'), (int)$gap);
+            $gap = sprintf(Gdn::translate('%s days later'), (int)$gap);
             $class = 'Days';
         } elseif ($gap < 60) {
-            $gap = t('1 month later');
+            $gap = Gdn::translate('1 month later');
             $class = 'Month';
         } elseif ($gap < 365) {
-            $gap = sprintf(t('%s months later'), (int)($gap / 30));
+            $gap = sprintf(Gdn::translate('%s months later'), (int)($gap / 30));
             $class = 'Months';
         } elseif ($gap < 720) {
-            $gap = t('1 year later');
+            $gap = Gdn::translate('1 year later');
             $class = 'Year';
         } else {
-            $gap = sprintf(t('%s years later'),  (int)($gap / 365));
+            $gap = sprintf(Gdn::translate('%s years later'),  (int)($gap / 365));
             $class = 'Years';
         }
 
